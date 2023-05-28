060e2b99411b59fb6159bed03c123fadd77f9dd0
Hirose-Koichi <96350637+Hirose-Koichi@users.noreply.github.com>
2023-05-28 15:33:28 +0300

    Delete markup directory

--- markup/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/Hirose-Koichi.github.io/",
-  plugins: [react()],
-  root: "src",
-});
